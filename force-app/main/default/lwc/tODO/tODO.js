8692bfcb15c11faac6fab0a3916c020a34ff6bff
Pardeep Pathania <pardeeppathania09@gmail.com>
2020-06-09 18:53:19 -0400

    Data population

--- force-app/main/default/lwc/tODO/tODO.js
+++ force-app/main/default/lwc/tODO/tODO.js
@@ -2,16 +2,14 @@ import { LightningElement,api,track,wire } from 'lwc';
 import loadData from '@salesforce/apex/TODOController.loadData';
 
 const COLS = [
-    { label: 'Name', fieldName: 'FirstName', editable: true },
-    { label: 'Decription', fieldName: 'Decription', editable: true },
-    { label: 'Reminder_Date', fieldName: 'Reminder_Date',type: 'date' },
-    { label: 'Due_Date', fieldName: 'Due_Date', type: 'date' },
-    { label: 'Comleted', fieldName: 'EmaComletedl', type: 'checkbox' }
+    { label: 'Name', fieldName: 'Name', editable: true },
+    { label: 'Description', fieldName: 'Description__c', editable: true },
+    { label: 'Reminder_Date', fieldName: 'Reminder_Date__c',type: 'date' },
+    { label: 'Due_Date', fieldName: 'Due_Date__c', type: 'date' },
+    { label: 'Completed', fieldName: 'Comleted__c', type: 'boolean' }
 ];
 
 export default class TODO extends LightningElement {
-    @api recordId;
-
     @track columns = COLS;
 
     @wire(loadData)
