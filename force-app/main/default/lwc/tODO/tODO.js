b6f36326ef9c413ef702e348e642d020b037ee80
Pardeep Pathania <pardeeppathania09@gmail.com>
2020-06-09 16:19:43 -0400

    ToDo App

--- force-app/main/default/lwc/tODO/tODO.js
+++ force-app/main/default/lwc/tODO/tODO.js
@@ -1,3 +1,19 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api,track,wire } from 'lwc';
+import loadData from '@salesforce/apex/TODOController.loadData';
 
-export default class TODO extends LightningElement {}
+const COLS = [
+    { label: 'Name', fieldName: 'FirstName', editable: true },
+    { label: 'Decription', fieldName: 'Decription', editable: true },
+    { label: 'Reminder_Date', fieldName: 'Reminder_Date',type: 'date' },
+    { label: 'Due_Date', fieldName: 'Due_Date', type: 'date' },
+    { label: 'Comleted', fieldName: 'EmaComletedl', type: 'checkbox' }
+];
+
+export default class TODO extends LightningElement {
+    @api recordId;
+
+    @track columns = COLS;
+
+    @wire(loadData)
+    todo;
+}
